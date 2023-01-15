51b2fbe7bc8d88e06464498b3b9c626d6ca14aa2
Alexandre E. Souza <alexandre@indev.net.br>
2023-01-14 21:39:32 -0300

    Update db.mixin.js

--- mixins/db.mixin.js
+++ mixins/db.mixin.js
@@ -1,19 +1,28 @@
+"use strict";
 
 const fs = require("fs");
+const DbService	= require("moleculer-db");
+const SqlAdpter = require("@indevweb/moleculer-db-adapter-sequelize");
 
 
 module.exports = {
-        dialect: "postgres",
-        username: "alexandre",
-        password: "It8KFcYm8HrmQa6Yn29M7w",
-        host: "blog-api-8104.7tt.cockroachlabs.cloud",
-        port: 26257,
-        database: "blog-api-8104.defaultdb",
-        dialectOptions:{
-            ssl:{
-                ca: fs.readFileSync("/home/alexandre/.postgresql/root.crt").toString(),
-            }
-        },
-        logging: true,
+		mixins: [DbService],
+        adapter: new SqlAdpter(
+        {
+            dialect: "postgres",
+            username: "alexandre",
+            password: "It8KFcYm8HrmQa6Yn29M7w",
+            host: "blog-api-8104.7tt.cockroachlabs.cloud",
+            port: 26257,
+            database: "blog-api-8104.defaultdb",
+            dialectOptions:{
+                ssl:{
+                    ca: fs.readFileSync("/home/alexandre/.postgresql/root.crt").toString(),
+                }
+            },
+            logging: true,
+    
+    }
+    )
 
-}
+};
